d9274d339fdab6a4ba5fd83fc27a2a065ecb816f
BrendaDrewanz <brendadrewanz@gmail.com>
2024-03-29 19:05:42 -0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AF1',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
